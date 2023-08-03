5d2e5cbd699188f0fc42645c2cbee8646ed31db7
cyran <gs.gouin@gmail.com>
2023-08-03 22:37:25 +0200

    QRCODE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import {NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels} from "@techiediaries/ngx-qrcode";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {faFacebook, faFacebookF, faFacebookMessenger, faFacebookSquare, faWhatsappSquare} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cyraqrcode';
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = '  ';
+  CreateQrcode(lien: string) {
+    console.log(lien);
+    this.value = lien;
+  }
+
+  share(){
+    if (navigator.share){
+      navigator.share({
+        title: 'yessssssssssssss',
+        url: 'google'
+      }).then(() => {
+        console.log('thanks for sharing ');
+      })
+        .catch(console.error);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {NgxQRCodeModule} from "@techiediaries/ngx-qrcode";
+import {FormsModule} from "@angular/forms";
+import {ShareModule} from "ngx-sharebuttons";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {ShareButtonsPopupModule} from "ngx-sharebuttons/popup";
+import {ShareButtonsModule} from "ngx-sharebuttons/buttons";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {ClipboardModule} from "ngx-clipboard";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    ShareButtonsModule,
+    NgxQRCodeModule,
+    ShareButtonsPopupModule,
+    ClipboardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
